bf178b11c2a6732d2e0abe6610b0976c51324227
Maxime Finette <mfinette@z3r6p5.42lyon.fr>
2023-10-15 18:16:56 +0200

    infinite loop patched

--- cpp01/ex04/sed.cpp
+++ cpp01/ex04/sed.cpp
@@ -6,7 +6,7 @@
 /*   By: mfinette <mfinette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 13:46:59 by mfinette          #+#    #+#             */
-/*   Updated: 2023/10/15 14:30:05 by mfinette         ###   ########.fr       */
+/*   Updated: 2023/10/15 18:16:06 by mfinette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,9 @@ void	Sed::replace(string s1, string s2)
 {
 	ifstream	input_str(this->_infile.c_str());
 	string		buffer;
-	size_t		i;
-	
+	size_t		pos = 0;
+	size_t		found;
+
 	if (!input_str.is_open())
 	{
 		cout << "can't open file : " << this->_infile << "\nexiting program" << endl;
@@ -37,11 +38,26 @@ void	Sed::replace(string s1, string s2)
 		cout << "file is empty : " << this->_infile << endl;
 		input_str.close();
 	}
-	ofstream	output_str(this->_outfile.c_str());
-	while ((i = buffer.find(s1)) != string::npos)
+	if (s1.length() == 0)
 	{
-		buffer.erase(i, s1.length());
-		buffer.insert(i, s2);
+		cout << "incorrect arguments :(" << endl;
+		input_str.close();
+		return ;
+	}
+	ofstream	output_str(this->_outfile.c_str());
+
+	while ((found = buffer.find(s1, pos)) != string::npos)
+	{
+		output_str << buffer.substr(pos, found - pos); // Copy content before the match
+
+		if (s1.length() == s2.length()) {
+			// If s1 and s2 have the same length, simply append s2.
+			output_str << s2;
+		} else {
+			// If s1 and s2 have different lengths, adjust the position accordingly.
+			output_str << s2;
+			pos = found + s1.length(); // Move the position past the match
+		}
 	}
 	output_str << buffer;
 	output_str.close();
