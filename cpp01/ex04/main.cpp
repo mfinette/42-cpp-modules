35843367992fc546deb14ad06dd20871399192ff
Maxime Finette <mfinette@z4r11p1.42lyon.fr>
2023-10-16 12:47:27 +0200

    final loop fix

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -5,19 +5,64 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfinette <mfinette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/15 13:40:35 by mfinette          #+#    #+#             */
-/*   Updated: 2023/10/15 14:09:52 by mfinette         ###   ########.fr       */
+/*   Created: 2023/10/16 11:52:04 by mfinette          #+#    #+#             */
+/*   Updated: 2023/10/16 12:18:04 by mfinette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "sed.hpp"
+#include <fstream>
+#include <iostream>
 
-int	main(int ac, char **av)
-{
-	if (ac != 4)
-		return ((cout << "incorrect argument number, exiting program" << endl), 1);
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::cout;
+using std::endl;
+using std::getline;
 
-	Sed	sed(av[1]);
-	sed.replace(av[2], av[3]);
-	return (0);
+void replace(const string &s1, const string &s2, ifstream &inputFile, ofstream &outputFile) {
+	string line;
+	while (getline(inputFile, line)) {
+		size_t pos = 0;
+		while ((pos = line.find(s1, pos)) != string::npos) {
+			line.erase(pos, s1.size());
+			line.insert(pos, s2);
+			pos += s2.size();
+		}
+		outputFile << line << endl;
+	}
+}
+
+int main(int argc, char **argv) {
+	string filename;
+
+	if (argc != 4)
+	{
+		cout << "Usage: ./sed <filename> <s1> <s2>" << endl;
+		return (1);
+	}
+	if (static_cast<string>(argv[2]).size() == 0)
+	{
+		cout << "s1 cannot be empty" << endl;
+		return (1);
+	}
+	filename = argv[1];
+	ifstream inputFile(filename.c_str());
+	if (!inputFile.is_open())
+	{
+		cout << "Input file" << endl;
+		return (1);
+	}
+
+	ofstream outputFile(filename.append(".replace").c_str(), std::ios_base::out | std::ios_base::trunc);
+	if (!outputFile.is_open())
+	{
+		inputFile.close();
+		cout << "Output file" << endl;
+		return 1;
+	}
+	replace(argv[2], argv[3], inputFile, outputFile);
+	inputFile.close();
+	outputFile.close();
+	return 0;
 }
